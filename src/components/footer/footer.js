643dfa5a916079f38deda1f10d94927dddb31516
bryanmej <bryanmej@gmail.com>
2020-10-31 13:42:30 -0600

    Maqueta Bankapp1.jpg

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,24 @@
 import React from "react";
+import "./footer.styles.css";
 
 const Footer = () => {
   return (
     <footer>
-      <p>
-        <span>Bankapp</span>
-        <span>Powered by Conecta Mexico</span>
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ipsam,
-        harum ullam
-      </p>
+      <div>
+        <p>
+          <span>Bankapp</span>
+          <span>Powered by Conecta Mexico</span>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
+          ipsam, harum ullam
+        </p>
+      </div>
+      <div>
+        <a href="#!" title="Bankapp">
+          Bankapp
+        </a>
+      </div>
     </footer>
   );
 };
